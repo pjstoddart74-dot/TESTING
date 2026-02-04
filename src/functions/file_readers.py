71f3795abe491dbd9b22d5e850628e1b6438166c
Paul Stoddart <paul.stoddart@balfourbeatty.com>
2026-02-04 11:36:44 +0000

    v1.00 testing

---
+++ src/functions/file_readers.py
@@ -0,0 +1,4 @@
+import pandas as pd         
+def read_csv_to_dataframe(filename):
+    df = pd.read_csv
+    return df
---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from src.functions.file_readers import read_csv_to_dataframe
+def test_read_csv():
+    #arrange
+    filename = "data/test_stock.csv"
+
+    #act
+
+    result = read_csv_to_dataframe(filename)
+
+    #assert
+    filetype = pd.DataFrame
+    assert filetype == type(result)   
